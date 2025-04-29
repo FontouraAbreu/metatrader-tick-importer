2542fb0a20d9ab39ecd77e7281e9511448d4435d
vinicius fontoura <v.abreu.fontoura@gmail.com>
2025-04-29 02:46:33 -0300

    [add] retrieving bova11 tickets but the list come as None

--- main.py
+++ main.py
@@ -3,26 +3,26 @@
 import MetaTrader5 as mt5
 from datetime import datetime
 import pandas as pd
+import pytz
 
 from config import LOGIN, PASSWORD, SERVER, TICKETS, RETRIEVE_TICKETS_SINCE
-
-print("Login: %s" % LOGIN)
-print("Password: %s" % PASSWORD)
-print("Server: %s" % SERVER)
-print("Tickets: %s" % TICKETS)
 
 
 def main():
     # Initialize MetaTrader 5 connection
-    if not mt5.initialize(login=LOGIN, password=PASSWORD, server=SERVER):
-        print("initialize() failed")
+    if not mt5.initialize():
         mt5.shutdown()
-        raise Exception(
-            "login failed, check your login credentials: %s" % mt5.last_error()
-        )
+        print("initialize() failed, error code =", mt5.last_error())
+        raise Exception("login failed")
 
+    print("MetaTrader 5 initialized successfully")
     # Get the current time
-    now = datetime.now()
+    timezone = pytz.timezone("America/Sao_Paulo")
+    utc_from = datetime(2020, 1, 10, tzinfo=timezone)
+    utc_to = datetime(2020, 1, 11, hour=13, tzinfo=timezone)
+
+    print("Retrieving tickets since %s" % utc_from)
+    print("Retrieving tickets until %s" % utc_to)
 
     # Create a DataFrame to store the results
     df = pd.DataFrame(columns=["Ticket", "Profit"])
@@ -30,15 +30,18 @@ def main():
     # Loop through each ticket and get the profit
     for ticket in TICKETS:
         try:
+            print("Retrieving profit for ticket %s" % ticket)
             candles = mt5.copy_rates_range(
-                ticket,
-                mt5.TIMEFRAME_M1,
-                now.replace(hour=0, minute=0, second=0, microsecond=0),
-                datetime(RETRIEVE_TICKETS_SINCE),
+                "BOVA11",
+                mt5.TIMEFRAME_M5,
+                utc_from,
+                utc_to,
             )
         except Exception as e:
             print("Error retrieving candles for ticket %s: %s" % (ticket, e))
             continue
+
+        print("Candles retrieved for ticket %s" % candles)
 
         if candles is None:
             print("No candles found for ticket %s" % ticket)
@@ -53,7 +56,7 @@ def main():
     mt5.shutdown()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     print("Starting script")
     try:
         main()
